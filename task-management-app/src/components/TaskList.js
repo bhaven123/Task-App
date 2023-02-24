079632d2ca4be9c470a805fcfc9bd7e0cbeebb5b
bhaven123 <naikbhaven11@gmail.com>
2023-02-24 13:57:35 -0400

    changed ui and added some functionality

--- task-management-app/src/components/TaskList.js
+++ task-management-app/src/components/TaskList.js
@@ -7,8 +7,52 @@ import {
   ListItemText,
   Typography,
   Button,
+  IconButton,
+  TextField,
+  Grid,
 } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import { useState } from "react";
+
 const TaskList = (props) => {
+  const [saveTask, setSaveTask] = useState(false);
+  const [editTask, setEditTask] = useState({
+    enteredPerson: {
+      breakfast: props.breakfast.assigned,
+      lunch: props.lunch.assigned,
+      dinner: props.dinner.assigned,
+    },
+    enteredDish: {
+      breakfast: props.breakfast.dish,
+      lunch: props.lunch.dish,
+      dinner: props.dinner.dish,
+    },
+  });
+
+  const [isEditingTask, setIsEditingTask] = useState({
+    editBreakfast: false,
+    editLunch: false,
+    editDinner: false,
+  });
+
+  const saveTaskHandler = (event) => {
+    event.preventDefault();
+    setSaveTask(true);
+    setIsEditingTask(false);
+  };
+
+  const editBreakfastHandler = () => {
+    setIsEditingTask({ ...isEditingTask, editBreakfast: true });
+  };
+
+  const editLunchHandler = () => {
+    setIsEditingTask({ ...isEditingTask, editLunch: true });
+  };
+
+  const editDinnerHandler = () => {
+    setIsEditingTask({ ...isEditingTask, editDinner: true });
+  };
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardContent>
@@ -17,31 +61,153 @@ const TaskList = (props) => {
         </Typography>
         <List>
           <ListItem>
-            <ListItemText
-              primary="Breakfast"
-              secondary={`Person: ${props.breakfast.assigned}, Dish: ${props.breakfast.dish}`}
-            />
+            {!isEditingTask.editBreakfast ? (
+              <ListItemText
+                primary="Breakfast"
+                secondary={`Person: ${props.breakfast.assigned}, Dish: ${props.breakfast.dish}`}
+              />
+            ) : (
+              <div>
+                <ListItemText primary="Breakfast" />
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      name="breakfast-assigned"
+                      id="breakfast-assigned-small"
+                      value={editTask.enteredPerson.breakfast}
+                      size="small"
+                      label="Person"
+                      onChange={(e) =>
+                        setEditTask((prevState) => {
+                          return {
+                            ...prevState,
+                            enteredPerson: { breakfast: e.target.value },
+                          };
+                        })
+                      }
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      name="breakfast-dish"
+                      id="breakfast-dish-small"
+                      value={editTask.enteredDish.breakfast}
+                      size="small"
+                      label="Dish"
+                      onChange={(e) =>
+                        setEditTask({
+                          ...editTask,
+                          enteredDish: { breakfast: e.target.value },
+                        })
+                      }
+                    />
+                  </Grid>
+                </Grid>
+              </div>
+            )}
+            <IconButton aria-label="edit" onClick={editBreakfastHandler}>
+              <EditIcon />
+            </IconButton>
           </ListItem>
           <ListItem>
-            <ListItemText
-              primary="Lunch"
-              secondary={`Person: ${props.lunch.assigned}, Dish: ${props.lunch.dish}`}
-            />
+            {!isEditingTask.editLunch ? (
+              <ListItemText
+                primary="Lunch"
+                secondary={`Person: ${props.lunch.assigned}, Dish: ${props.lunch.dish}`}
+              />
+            ) : (
+              <div>
+                <ListItemText primary="Lunch" />
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      name="lunch-assigned"
+                      id="lunch-assigned-small"
+                      value={editTask.enteredPerson.lunch}
+                      size="small"
+                      label="Person"
+                      onChange={(e) =>
+                        setEditTask({
+                          ...editTask,
+                          enteredPerson: { lunch: e.target.value },
+                        })
+                      }
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      name="lunch-dish"
+                      id="lunch-dish-small"
+                      value={editTask.enteredDish.lunch}
+                      size="small"
+                      label="Dish"
+                      onChange={(e) =>
+                        setEditTask({
+                          ...editTask,
+                          enteredDish: { lunch: e.target.value },
+                        })
+                      }
+                    />
+                  </Grid>
+                </Grid>
+              </div>
+            )}
+            <IconButton aria-label="edit" onClick={editLunchHandler}>
+              <EditIcon />
+            </IconButton>
           </ListItem>
           <ListItem>
-            <ListItemText
-              primary="Dinner"
-              secondary={`Person: ${props.dinner.assigned}, Dish: ${props.dinner.dish}`}
-            />
+            {!isEditingTask.editDinner ? (
+              <ListItemText
+                primary="Dinner"
+                secondary={`Person: ${props.dinner.assigned}, Dish: ${props.dinner.dish}`}
+              />
+            ) : (
+              <div>
+                <ListItemText primary="Dinner" />
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      name="dinner-assigned"
+                      id="dinner-assigned-small"
+                      value={editTask.enteredPerson.dinner}
+                      size="small"
+                      label="Person"
+                      onChange={(e) =>
+                        setEditTask({
+                          ...editTask,
+                          enteredPerson: { dinner: e.target.value },
+                        })
+                      }
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      name="dinner-dish"
+                      id="dinner-dish-small"
+                      value={editTask.enteredDish.dinner}
+                      size="small"
+                      label="Dish"
+                      onChange={(e) =>
+                        setEditTask({
+                          ...editTask,
+                          enteredDish: { dinner: e.target.value },
+                        })
+                      }
+                    />
+                  </Grid>
+                </Grid>
+              </div>
+            )}
+            <IconButton aria-label="edit" onClick={editDinnerHandler}>
+              <EditIcon />
+            </IconButton>
           </ListItem>
         </List>
       </CardContent>
       <CardActions>
-        <Button variant="contained" size="small">
-          Edit
-        </Button>
-        <Button variant="contained" size="small">
-          Delete
+        <Button variant="contained" size="small" onClick={saveTaskHandler}>
+          Save
         </Button>
       </CardActions>
     </Card>
---
+++ task-management-app/src/components/data.js
@@ -0,0 +1,130 @@
+const DATA = [
+  {
+    id: 1,
+    day: "Monday",
+    task: {
+      Breakfast: {
+        assigned: "Bhaven",
+        dish: "Poha",
+      },
+      Lunch: {
+        assigned: "Dhairya",
+        dish: "Bhinda",
+      },
+      Dinner: {
+        assigned: "Shubham",
+        dish: "Chole Chana",
+      },
+    },
+  },
+  {
+    id: 2,
+    day: "Tuesday",
+    task: {
+      Breakfast: {
+        assigned: "Dhairya",
+        dish: "Omelette",
+      },
+      Lunch: {
+        assigned: "Shubham",
+        dish: "Sev Tameta",
+      },
+      Dinner: {
+        assigned: "Bhaven",
+        dish: "Quesadia",
+      },
+    },
+  },
+  {
+    id: 3,
+    day: "Wednesday",
+    task: {
+      Breakfast: {
+        assigned: "Shubham",
+        dish: "Masala Oats",
+      },
+      Lunch: {
+        assigned: "Bhaven",
+        dish: "Paneer Bhurji",
+      },
+      Dinner: {
+        assigned: "Dhairya",
+        dish: "Pizza",
+      },
+    },
+  },
+  {
+    id: 4,
+    day: "Thursday",
+    task: {
+      Breakfast: {
+        assigned: "Bhaven",
+        dish: "Cereal",
+      },
+      Lunch: {
+        assigned: "Dhairya",
+        dish: "Rasa wada Bateta",
+      },
+      Dinner: {
+        assigned: "Shubham",
+        dish: "Bhartu",
+      },
+    },
+  },
+  {
+    id: 5,
+    day: "Friday",
+    task: {
+      Breakfast: {
+        assigned: "Dhairya",
+        dish: "Upma",
+      },
+      Lunch: {
+        assigned: "Shubham",
+        dish: "Mushroom Methi Malai",
+      },
+      Dinner: {
+        assigned: "Bhaven",
+        dish: "Pasta",
+      },
+    },
+  },
+  {
+    id: 6,
+    day: "Saturday",
+    task: {
+      Breakfast: {
+        assigned: "Shubham",
+        dish: "Smoothie",
+      },
+      Lunch: {
+        assigned: "Dhairya",
+        dish: "Kathod",
+      },
+      Dinner: {
+        assigned: "Bhaven",
+        dish: "Chicken Curry",
+      },
+    },
+  },
+  {
+    id: 7,
+    day: "Sunday",
+    task: {
+      Breakfast: {
+        assigned: "Bhaven",
+        dish: "Egg Burger",
+      },
+      Lunch: {
+        assigned: "Shubham",
+        dish: "Kathiywadi",
+      },
+      Dinner: {
+        assigned: "Dhairya",
+        dish: "Masala Rice",
+      },
+    },
+  },
+];
+
+export default DATA;
